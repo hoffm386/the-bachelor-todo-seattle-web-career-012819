89efdde6fd2360ddf974a93b27ef8e80340e507b
Erin Hoffman <ehoffman@Erins-MacBook-Air.local>
2019-01-29 18:48:31 -0800

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,24 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  contestant = data[season].find{|contestant| contestant["status"] == "Winner"}
+  return contestant["name"].split(" ")[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant = data.values.flatten.find{|contestant| contestant["occupation"] == occupation}
+  return contestant["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  return data.values.flatten.select{|contestant| contestant["hometown"] == hometown}.length
 end
 
 def get_occupation(data, hometown)
-  # code here
+  contestant = data.values.flatten.find{|contestant| contestant["hometown"] == hometown}
+  return contestant["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  contestant_ages = data[season].collect{|contestant| contestant["age"].to_f}
+  # answer rounded to nearest integer
+  return (contestant_ages.reduce(:+) / contestant_ages.length).round
 end
